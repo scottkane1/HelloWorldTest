db3f32fba3e73111069651707f6be77b30197370
scottkane <scottkane1@gmail.com>
2022-11-21 18:57:48 +0200

    Lagoudaki text

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,10 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont(juce::Font(90.0f));
+    g.setFont(juce::Font(30.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello my little cutie rabbit :)", getLocalBounds(), juce::Justification::centred, true);
+  
 }
 
 void MainComponent::resized()
